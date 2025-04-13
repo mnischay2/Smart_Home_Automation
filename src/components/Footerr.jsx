27ca02dc74efa4d1732e8c2bf7c86f6c1c4d8891
mnischay2 <mnischay2@gmail.com>
2025-04-14 01:45:15 +0530

    changes

---
+++ src/components/Footerr.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footerr.css'
+
+function footer() {
+  return (
+
+    <div id='footer'>
+        <p>Home Automation || All Rights reserved </p>
+    </div>
+
+  )
+}
+
+export default footer
